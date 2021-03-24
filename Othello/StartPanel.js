92f821ed07f17d8b0a4341a31da2f89aaa766a85
risakambe <66462143+risakambe@users.noreply.github.com>
2021-03-24 08:32:29 +0100

    Othello web application without socket

---
+++ Othello/StartPanel.js
@@ -0,0 +1,29 @@
+// StartPanel
+
+function st_start()
+{
+    mp.ctx.clearRect(0, 0, mp.canvas.width, mp.canvas.height);
+    mp.ctx.lineWidth = 2;
+    mp.ctx.font = "70px Monotype Corsiva";
+
+    mp.ctx.textBaseline = "middle"; //y
+    mp.ctx.textAlign = "center";  //x
+    mp.ctx.fillStyle = "#AAA";
+    mp.ctx.fillText("Welcome to", mp.canvas.width/2+4, mp.canvas.height/3);
+
+    mp.ctx.fillStyle = "blue";
+    mp.ctx.fillText("Welcome to", mp.canvas.width/2, mp.canvas.height/3);
+
+    mp.ctx.fillStyle = "#AAA";
+    mp.ctx.fillText("Othello !!",mp.canvas.width/2+4, mp.canvas.height/3+80);
+
+    mp.ctx.fillStyle = "blue";
+    mp.ctx.fillText("Othello !!",mp.canvas.width/2, mp.canvas.height/3+80);
+
+    //button control
+    document.getElementById('method').style.display = "";
+    document.getElementById('start').style.display = "";
+    document.getElementById('ta').style.display = "none";
+    document.getElementById('first').style.display = "none";
+    document.getElementById('finish').style.display = "none";
+}
